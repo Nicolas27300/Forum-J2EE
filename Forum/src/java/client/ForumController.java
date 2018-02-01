da832dd05b88f51e916c924f6d7544e1338fcc5a
BuildTools <unconfigured@null.spigotmc.org>
2018-02-01 19:49:59 +0100

    Update

--- Forum/src/java/client/ForumController.java
+++ Forum/src/java/client/ForumController.java
@@ -1,21 +1,42 @@
 package client;
 
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import metier.ForumService;
 import metier.MetierFactory;
+import metier.entitys.Forum;
 
 @ManagedBean
 @ViewScoped
 public class ForumController implements Serializable {
     
     ForumService forumSrv;
+    Forum forum;
     
     @PostConstruct
     public void init(){
         this.forumSrv = MetierFactory.getForumService();
+        HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        long id = Long.parseLong(req.getParameter("id"));
+        try {
+            this.forum = this.forumSrv.getById(id);
+        } catch (Exception ex) {
+            Logger.getLogger(ForumController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
+    public Forum getForum() {
+        return forum;
+    }
+
+    public void setForum(Forum forum) {
+        this.forum = forum;
+    }
+ 
 }
--- Forum/src/java/client/MembreController.java
+++ Forum/src/java/client/MembreController.java
@@ -1,11 +1,15 @@
 package client;
 
 import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import metier.MembreService;
 import metier.MetierFactory;
 import metier.entitys.Membre;
@@ -17,6 +21,7 @@ public class MembreController implements Serializable {
     MembreService membreSrv;
     private String pseudo;
     private String password;
+    private String password_confirm;
     private String mail;
     
     @PostConstruct
@@ -28,10 +33,14 @@ public class MembreController implements Serializable {
     public void add(){
         Membre membre = new Membre();
         membre.setPseudo(this.pseudo);
-        membre.setPassword(this.password);
+        membre.setPassword(this.encodeMd5(this.password));
         membre.setMail(this.mail);
         try {
-            this.membreSrv.add(membre);
+            if (this.password.equals(this.password_confirm)){
+                this.membreSrv.add(membre);
+            } else {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Erreur", "Mot de passe non identique"));
+            }
         } catch (Exception ex) {
             Logger.getLogger(MembreController.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -53,12 +62,41 @@ public class MembreController implements Serializable {
         this.password = password;
     }
 
+    public String getPassword_confirm() {
+        return password_confirm;
+    }
+
+    public void setPassword_confirm(String password_confirm) {
+        this.password_confirm = password_confirm;
+    }
+    
     public String getMail() {
         return mail;
     }
 
     public void setMail(String mail) {
         this.mail = mail;
+    }
+    
+     public String encodeMd5(String mdp) {
+        byte[] uniqueKey = mdp.getBytes();
+        byte[] hash = null;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(uniqueKey);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(MembreController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        StringBuilder hashString = new StringBuilder();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(hash[i]);
+            if (hex.length() == 1) {
+                hashString.append('0');
+                hashString.append(hex.charAt(hex.length() - 1));
+            } else {
+                hashString.append(hex.substring(hex.length() - 2));
+            }
+        }
+        return hashString.toString();
     }
     
 }
